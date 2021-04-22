139157f04098e02423981a110f7b7c63f239b666
AkumuYuma <manumanu1355@gmail.com>
2021-04-22 16:01:25 +0200

    Aggiunto Procfile

--- server.py
+++ server.py
@@ -77,4 +77,4 @@ def delSession():
 
 if __name__ == "__main__":
     # Per esporlo alla rete modificare host = "0.0.0.0"
-    app.run(host = '0.0.0.0', port = 5000, debug = False)
+    app.run(host = '0.0.0.0', debug = False)
